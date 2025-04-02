d61ccc9fb199c1a44be1c9300c461ec2b406690f
TristanAckermann <tristan.ackermann@outlook.com>
2025-04-02 09:30:07 +0200

    fist commit

---
+++ src/main/java/ch/noseryoung/projekt295/Projekt295Application.java
@@ -0,0 +1,13 @@
+package ch.noseryoung.projekt295;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Projekt295Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Projekt295Application.class, args);
+    }
+
+}
---
+++ src/test/java/ch/noseryoung/projekt295/Projekt295ApplicationTests.java
@@ -0,0 +1,13 @@
+package ch.noseryoung.projekt295;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Projekt295ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
